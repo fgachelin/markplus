f1444c0e4d5e8d9d85a689da5da70ad4e9ef9648
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-10-17 14:36:49 +0200

    Update mkp.js

--- mkp.js
+++ mkp.js
@@ -2,3 +2,5 @@ console.log("Grammar")
 console.log("--- <= ---")
 console.log("titre_page <= sop . nl --- nl")
 console.log("titre_paragraphe <= nl # . nl")
+
+document.body.innerHTML = "Ctrl Maj I"
